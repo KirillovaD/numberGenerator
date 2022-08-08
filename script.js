f130b9331a335882abf85034548491f898c68351
KirillovaD <sdaria@inbox.ru>
2022-08-08 18:10:55 +0300

    add extra feature

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const btn = document.querySelector('button');
+const numbersArray = document.querySelector('.number');
+const minNumber =  document.querySelector('.min');
+const maxNumber =  document.querySelector('.max');
+const sumNumber = document.querySelector('.sum');
+const averageNumber = document.querySelector('.average');
+const multiNumber = document.querySelector('.multi');
 
 btn.addEventListener('click', generateRandomNumberArray);
 
@@ -11,23 +17,30 @@ function generateNumber(min, max){
 
 function generateRandomNumberArray(){
   let randomNumberArray = []; //создаем массив для скалдывания чисел
+  numbersArray.textContent = '';
+  minNumber.textContent = '';
+  maxNumber.textContent = '';
+  sumNumber.textContent = '';
+  averageNumber.textContent = '';
+
+
 
   let sum = 0; //создаем сумму
   let average; 
   let multi = 1; //произведение
   for (let i = 0; i < 10; i++){ //проходимся по массиву и тк длиный массива нет берем колчество чисел в ряду
     let number = generateNumber(-10, 10); //вызываем функция генерации чисел в нужном нам диапозоне
-    document.querySelector('.number').textContent += randomNumber + ', '; //выводим числа
+    numbersArray.textContent += randomNumber + ', '; //выводим числа
     sum += number;
     multi = multi * number;
     average = sum/10; 
     randomNumberArray.push(number); //добавляем число массив сразу поле его генерации
   }
-  document.querySelector('.min').textContent = Math.min(...randomNumberArray); //нахождение минимума
-  document.querySelector('.max').textContent = Math.max(...randomNumberArray); // нахождение максимума
-  document.querySelector('.sum').textContent = sum; 
-  document.querySelector('.average').textContent = average;
-  document.querySelector('.multi').textContent = multi;
+  minNumber.textContent = Math.min(...randomNumberArray); //нахождение минимума
+  maxNumber.textContent = Math.max(...randomNumberArray); // нахождение максимума
+  sumNumber.textContent = sum; 
+  averageNumber.textContent = average;
+  multiNumber.textContent = multi;
 
 }
 
